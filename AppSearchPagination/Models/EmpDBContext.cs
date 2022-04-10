fa20d3d5d9f6c23466f3f7f369d1e9f7ffe8541d
tanver68 <16101068@uap-bd.edu>
2022-04-10 10:11:45 +0600

    Add project files.

---
+++ AppSearchPagination/Models/EmpDBContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AppSearchPagination.Models
+{
+    public partial class EmpDBContext : DbContext
+    {
+        public EmpDBContext()
+        {
+        }
+
+        public EmpDBContext(DbContextOptions<EmpDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                //optionsBuilder.UseSqlServer("Server=WIN-FS4U3DFC5B3\\SQLEXPRESS;Database=EmpDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AppSearchPagination/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AppSearchPagination.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeStatus { get; set; }
+        public decimal Salary { get; set; }
+        public string PayBasis { get; set; }
+        public string PositionTitle { get; set; }
+    }
+}
